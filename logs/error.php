50a4d189fe2ef31642ce8a5ef2844006f1450f4b
root <root@ip-172-31-3-78.us-west-2.compute.internal>
2014-12-11 22:17:18 +0000

    first commit

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-12-09 21:24:08 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-12-09T21:24:08+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-12-09T22:05:58+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-12-10T20:50:10+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
